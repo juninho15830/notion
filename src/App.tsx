f3d6b91b2044e3cc33c7bdd6dfbcc22e660bddcd
juninho15830 <juninho15830@gmail.com>
2024-07-24 11:20:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
       <div className="bg-white">
         <main className="p-4">
         <section className=" flex flex-col mt-1 max-w-[720px] m-auto leading-7">
-            <h1 className="text-[39px] font-bold border-b-[1px] border-gray-300 pb-4 my-4">Front-end developer test project</h1>
+            <h1 className="text-[39px] font-bold border-b-[1px] border-gray-300 pb-8 my-4">Front-end developer test project</h1>
             <p>Your goal is to make a page that looks exactly like this one, and the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter. </p>
         </section>
           <Editor />
